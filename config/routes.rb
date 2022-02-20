fbd4587a97e9aa57bcf12bffac943d5e28a0eb71
akiogitgit <akimaron2828@icloud.com>
2022-02-20 16:11:04 +0900

    can't destory!

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # get "boards/new", to: "boards#new" # 新規作成
   # post "boards", to: "boards#create" # form送信
   # get "boards/:id", to: "boards#show" # 個別ページ
-  # resources :boards # これで上の全てに対応
+  resources :boards # これで上の全てに対応
 
   get 'user/new'
   root "boards#index"
   #                        一覧      新規作成 POST  動的    更新    POST
-  resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy] # これ以外は使わない
+  # resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy] # これ以外は使わない
 end
