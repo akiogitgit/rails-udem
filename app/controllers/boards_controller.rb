946a71630607f83d7e4bc9b360e51067ca0947eb
akiogitgit <akimaron2828@icloud.com>
2022-02-20 20:35:39 +0900

    can deleet show

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,7 @@
 class BoardsController < ApplicationController
+    # 最初に指定したメソッドで実行(このメソッドの中に@boardの文が入る)
+    before_action :set_board, only: %i[show edit update destroy]
+
     def index
         @board = Board.all
     end
@@ -14,6 +17,7 @@ class BoardsController < ApplicationController
         # newで登録したやつを、POSTで受け取って作成
         # paramsにPOSTで送信されたデータが保存される
         # params[:board]に必要なやつ
+        @board = Board.find(params[:id])
         Board.create(board_params)
         redirect_to root_path
     end
@@ -21,25 +25,25 @@ class BoardsController < ApplicationController
     # 動的(get)
     def show
         # binding.pry
-        @board = Board.find(params[:id])
+        # @board = Board.find(params[:id])
     end
 
     # 編集edit(get), update(post)
     def edit
-        @board = Board.find(params[:id])
+        # @board = Board.find(params[:id])
     end
 
     def update
-        board = Board.find(params[:id])
-        board.update(board_params)
-        redirect_to root_path
+        # @board = Board.find(params[:id])
+        @board.update(board_params)
+        redirect_to board
     end
     
     def destroy
-        # board = Board.find(params[:id])
-        @board.destroy
-        # redirect_to boards_path
-        redirect_to board
+        @board = Board.find(params[:id])
+        # board.destroy
+        @board.delete # これと、"#{@board.id}",@boardの組み合わせできた！！！！
+        redirect_to boards_path # boardが出来ないなら、boards_path
     end
 
     # これより下に、普通のは置かない
@@ -49,4 +53,8 @@ class BoardsController < ApplicationController
         params.require(:board).permit(:name, :title, :body)
     end
 
+    def set_board
+        @board = Board.find(params[:id])
+    end
 end
+
