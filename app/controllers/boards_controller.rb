66a83d22290d07263a0da81c50072dee2b622082
akiogitgit <akimaron2828@icloud.com>
2022-02-20 15:52:55 +0900

    パーシャル

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,6 +3,7 @@ class BoardsController < ApplicationController
         @board = Board.all
     end
 
+    # 新規作成 new(get), create(post)
     def new
         @board = Board.new
         # binding.pry
@@ -11,18 +12,37 @@ class BoardsController < ApplicationController
     # filterを掛けるために、strong parameter
     def create
         # newで登録したやつを、POSTで受け取って作成
-        Board.create(board_params)
-        # binding.pry
         # paramsにPOSTで送信されたデータが保存される
         # params[:board]に必要なやつ
+        Board.create(board_params)
+        redirect_to root_path
     end
 
+    # 動的(get)
     def show
         # binding.pry
         @board = Board.find(params[:id])
     end
+
+    # 編集edit(get), update(post)
+    def edit
+        @board = Board.find(params[:id])
+    end
+
+    def update
+        board = Board.find(params[:id])
+        board.update(board_params)
+        redirect_to root_path
+    end
     
-    # privateより下に、普通のは置かない
+    def destroy
+        # board = Board.find(params[:id])
+        @board.destroy
+        # redirect_to boards_path
+        redirect_to board
+    end
+
+    # これより下に、普通のは置かない
     private
     
     def board_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  # get "boards", to: "boards#index" # 一覧
+  # get "boards/new", to: "boards#new" # 新規作成
+  # post "boards", to: "boards#create" # form送信
+  # get "boards/:id", to: "boards#show" # 個別ページ
+  # resources :boards # これで上の全てに対応
 
+  get 'user/new'
   root "boards#index"
-  get "boards", to: "boards#index" # 一覧
-  get "boards/new", to: "boards#new" # 新規作成
-  post "boards", to: "boards#create" # form送信
-  get "boards/:id", to: "boards#show" # 個別ページ
+  #                        一覧      新規作成 POST  動的    更新    POST
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy] # これ以外は使わない
 end
