c1ed543bf5e331d072dce3ee4628cac887e5e8d6
akiogitgit <akimaron2828@icloud.com>
2022-02-24 09:41:36 +0900

    create redirect ok, add flash msg CUD

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,7 @@
 class BoardsController < ApplicationController
 
     # 最初に指定したメソッドで実行(このメソッドの中に@boardの文が入る)
-    before_action :set_board, only: %i[show create edit update destroy]
+    before_action :set_board, only: %i[show edit update destroy]
 
     def index
         # @board = Board.all
@@ -13,45 +13,41 @@ class BoardsController < ApplicationController
         @board = Board.new
         # binding.pry
     end
-
-    # filterを掛けるために、strong parameter(board_params)
+    
+    # newで登録したやつを、POSTで受け取って作成
+    # paramsにPOSTで送信されたデータが保存される
+    # params[:board]に必要なやつが入っている
+    # flashのnoticeというキーに値を格納
     def create
-        # newで登録したやつを、POSTで受け取って作成
-        # paramsにPOSTで送信されたデータが保存される
-        # params[:board]に必要なやつ
-        @board = Board.find(params[:id])
         Board.create(board_params)
-        # flashの任意のキーに値を格納
-        flash[:notice] = "「#{@board.title}」の掲示板を作成しました。"
-        redirect_to boards_path
+        flash[:notice] = "「#{params[:board][:title]}」の掲示板を作成しました。"
+        redirect_to root_path
     end
 
-    # 動的(get)
+    # 動的(get) 個別ページ
     def show
-        # @board = Board.find(params[:id])
     end
 
     # 編集edit(get), update(post)
     def edit
-        # @board = Board.find(params[:id])
     end
 
     def update
-        # @board = Board.find(params[:id])
         @board.update(board_params)
         flash[:notice] = "「#{@board.title}」の掲示板を更新しました。"
         redirect_to root_path
     end
     
     def destroy
-        # @board = Board.find(params[:id])
         @board.destroy # deleteでもいける
+        flash[:notice] = "「#{@board.title}」の掲示板を削除しました。"
         redirect_to boards_path # boardが出来ないなら、boards_path
     end
 
     # これより下に、普通のは置かない
     private
     
+    # 必要ないのを受け取らないように、strong parameterで制限する
     def board_params
         params.require(:board).permit(:name, :title, :body)
     end
