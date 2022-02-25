32ac030880014fc5bc36604e872d5c36b95b0efa
akiogitgit <akimaron2828@icloud.com>
2022-02-25 16:15:07 +0900

    add has_many

--- app/models/board.rb
+++ app/models/board.rb
@@ -12,6 +12,7 @@
 class Board < ApplicationRecord
   has_many :comments
   has_many :board_tag_relations
+  has_many :tags, through: :board_tag_relations
   #                必須　　　　　　　文字数
   validates :name, presence: true, length: {maximum: 20}
   validates :title, presence: true, length: {maximum: 30}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,4 +9,5 @@
 #
 class Tag < ApplicationRecord
   has_many :board_tag_relations
+  has_many :boards, through: :board_tag_relations
 end
