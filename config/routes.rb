56e76f2863affc81849692fe1567d704c7d1ef7f
Akio Tanaka <akimaron2828@icloud.com>
2022-02-28 10:18:47 +0900

    add sessions controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,24 @@
 #
 
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root "home#index"
+
+  # to でpath名を変えられる
+  post "login", to: "sessions#create" # 当然post,deleteとかも使える
+  delete "logout", to: "sessions#destroy"
+  get "mypage", to: "users#me"
 
   #     パス名省略　　　アクション  ->boards_path, boards_new_pathで使える
   # get "boards", to: "boards#index" # 一覧
   # get "boards/new", to: "boards#new" # 新規作成
   # post "boards", to: "boards#create" # form送信
   # get "boards/:id", to: "boards#show" # 個別ページ
-  resources :boards # これで上の全てに対応
-  resources :comments, only: %i[create destroy]
-  resources :tags, only: %i[index create destroy]
 
-  #                        一覧      新規作成 POST  動的    更新    POST
-  # resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy] # これだけ使用する場合
+  # resources は名前が固定されるが、一気にCRUDに必要なpathを生成
+  resources :boards # これで上の全てに対応
+  resources :comments, only: %i[create destroy] # onlyでこれだけ使う
+  resources :tags, only: %i[index create destroy]
+  resources :users, only: %i[new create]
+  # resources :sessions, only: %i[create destroy] # 名前を変えたいから上に書く
 
 end
