7b0149940950bbc5eeaae41476b1522c64b9056d
Akio Tanaka <akimaron2828@icloud.com>
2022-02-28 14:54:41 +0900

    add user controller view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new(flash[:user])
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id # セッションに保存
+      redirect_to mypage_path, flash: { notice: "ユーザーを登録しました" }
+    else
+      redirect_to new_user_path, flash: {
+        user: user,
+        error: user.errors.full_messages
+      }
+    end
   end
 
   def me
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,22 @@
 #  index_users_on_name  (name) UNIQUE
 #
 class User < ApplicationRecord
+  # password, password_confirmation(確認用)の仮想的なカラムが追加される
+  has_secure_password
+
+  validates :name ,
+    presence: true,
+    uniqueness: true,
+    length: {maximum: 16},
+    format: {
+      with: /\A[a-z0-9]+\z/, # //内に書く。\A,\zが始まりと終わり
+      message: "小文字英数字で入力してください"
+    }
+  
+  validates :password,
+    uniqueness: true,
+    length: { minimum: 6, maximum: 30} # minimumならpresenceいらん
+  
+  
+  
 end
