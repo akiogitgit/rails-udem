653a3591bc4077b47d6995d37f8d33a32274ef0e
Akio Tanaka <akimaron2828@icloud.com>
2022-03-25 09:44:37 +0900

    add Schemafile

--- db/migrate/20220313003947_add_publish_to_boards.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishToBoards < ActiveRecord::Migration[7.0]
-  def change
-    # add_column :boards, :published, :boolean, default: true
-  end
-end
--- db/migrate/20220324082415_add_published_to_boards.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedToBoards < ActiveRecord::Migration[7.0]
-  def change
-    add_column :boards, :published, :boolean, default: true
-  end
-end
